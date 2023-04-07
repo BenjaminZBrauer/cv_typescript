f9e4c9aea86007e33d46a465889f4cb66fdcddc0
DavieJ0nes <ben@flowsa.com>
2023-04-07 17:38:33 +0200

    dev mode setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 import typescript from "@rollup/plugin-typescript"
 
+function operatingMode() {
+    return !!(process.argv.find(el => el == "--config-dev"))
+}
 export default [
     {
         input: "src/ts/app.ts",
         output: {
             dir: "public/js",
             format: "esm",
-            sourcemap:" true"
+            sourcemap: operatingMode()
         },
         plugins: [
             typescript({
