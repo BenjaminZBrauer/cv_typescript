bc813c494d8316da0fffb3d1f73514f245f71a01
DavieJ0nes <ben@flowsa.com>
2023-04-07 16:49:15 +0200

    rollup fix required tslib

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
-import Foo from "./Foo.js";
+class Foo {
+    constructor() {
+        console.log("this is Foo");
+        console.log("this is Foo");
+        console.log("this is Foo");
+        console.log("this is Foo");
+    }
+}
+
 new Foo();
 console.log("ts");
+//# sourceMappingURL=app.js.map
--- src/ts/Foo.ts
+++ src/ts/Foo.ts
@@ -1,9 +1,6 @@
 export default class Foo {
     constructor() {
         console.log("this is Foo");
-        console.log("this is Foo");
-        console.log("this is Foo");
-        console.log("this is Foo");
-
+        
     }
 }
